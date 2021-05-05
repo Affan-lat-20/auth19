3b38b37cf62e0f00568006c5d21dcf88619fba37
Affan-lat-20 <72376364+Affan-lat-20@users.noreply.github.com>
2021-05-05 21:26:54 +0500

    backedup-changes

--- controllers/YoutubeController.js
+++ controllers/YoutubeController.js
@@ -33,3 +33,43 @@ exports.addyoutubeData = async(req,res,next)=>{
         res.send(youtubedata);
         });
        }
+
+
+       exports.youtubeedit =  function (req,res){
+        var conditions ={_id: req.params.id};
+       
+        YoutubeData.updateOne(conditions, req.body)   
+            .then(doc =>{
+                if(!doc){
+                    return res.status(404).end();}
+                    return res.status(200).json(doc);
+                })
+                .catch(err => next(err));
+            
+        
+       
+        }
+
+        exports.getallyoutubedata=async(req , res)=> {
+            YoutubeData.find({}).then(function (YoutubeData) {
+            res.send(YoutubeData);
+            });
+           }
+    
+           
+    exports.getyouspec = async(req,res,next)=>{
+        // BrandEmployee.findOne(req.query)
+        let query;
+        let result = JSON.stringify(req.query);
+        result = result.replace(/\b(gt|gte|lt|lte|in)\b/g,match => `$${match}`);
+        console.log(result);
+    
+        query = YoutubeData.find(JSON.parse(result));
+        
+        const you = await query
+        res
+        .status(200)
+        .json(you);
+        
+    }
+    
